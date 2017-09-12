f7948ada8f187e29362870297f05f8acd0c2e7b6
akang3519 <akang3519@gmail.com>
2017-09-12 19:49:13 -0400

    statistics module

---
+++ Statistics.py
@@ -0,0 +1,8 @@
+import statistics
+
+numArray = [1,2,3,4,5]
+print("The set of data is ", numArray)
+
+print("The mean value of the data is", statistics.mean(numArray))
+print("The medium of the data is", statistics.median(numArray))
+print("The standard deviation is", statistics.stdev(numArray))
