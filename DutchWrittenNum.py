c5ed83fe70550af140d2d581af767333921c60a5
akang3519 <akang3519@gmail.com>
2017-09-28 09:45:25 -0400

    Converts an integer to the dutch version

--- DutchWrittenNum.py
+++ DutchWrittenNum.py
@@ -1,4 +1,4 @@
-from telwoord import cardinal, ordinal
+import telwoord
 
 boo = True
 
@@ -6,12 +6,12 @@ while(boo):
     choice = int(input("Enter 1 for cardinal translation, 2 for ordinal translation, or 0 to quit: "))
     if choice == 1:
         number = int(input("Enter a number to translate to cardinal Dutch Written Form (0 to quit): "))
-        print(cardinal(number, friendly = False))
+        print(telwoord.cardinal(number, friendly = False))
         if(number == 0):
             break;
     elif choice == 2:
         number = int(input("Enter a number to translate to ordinal Dutch Written Form (0 to quit): "))
-        print(ordinal(number, friendly = False))
+        print(telwoord.ordinal(number, friendly = False))
         if(number == 0):
             break;
     elif choice == 0:
